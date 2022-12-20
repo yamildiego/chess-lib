c898e79411718e58dcd762287a34fbcd4bdf6f4a
yamildiego <yamildiego@gmail.com>
2022-12-20 16:42:25 +1100

    edit passant only allow with contiguous squares

--- src/movements/perPiece/enPassantMovement.ts
+++ src/movements/perPiece/enPassantMovement.ts
@@ -12,7 +12,12 @@ const enPassantMovement = (board: Array<Array<PieceType | null>>, item: PieceTyp
     let positionTo = tPosSN(movements[1]);
     let opponent = board[positionTo.x][positionTo.y];
 
-    if (opponent !== null && item.color !== opponent.color && opponent.type == TypeOfPiece.PAWN) {
+    if (
+      opponent !== null &&
+      item.color !== opponent.color &&
+      opponent.type == TypeOfPiece.PAWN &&
+      (positionTo.y - 1 == position.y || positionTo.y + 1 == position.y)
+    ) {
       if (item.color == Color.WHITE && positionFrom.x == 6 && positionTo.x == 4 && position.x == 4)
         enPassantMovement = tPosNS({ x: 5, y: positionTo.y });
       if (item.color == Color.BLACK && positionFrom.x == 1 && positionTo.x == 3 && position.x == 3)
